b605ee918fcfb82d130fdc8d4f4e04009f8a5ca5
Itacir Gabral <itacirgabral@gmail.com>
2022-07-31 19:58:00 -0400

    fix projetos link

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
             <Link to="/">Home</Link>
           </li>
           <li className={styles.item}>
-            <Link to="/project">Projetos</Link>
+            <Link to="/projects">Projetos</Link>
           </li>
           <li className={styles.item}>
             <Link to="/contact">Contato</Link>
